fa7c51a3ca198a34074bf28da1854ad0aa559696
Alex Kramer <kramer612@gmail.com>
2019-08-14 16:59:58 -0400

    complete- no repeated element

--- TwoSumLC.js
+++ TwoSumLC.js
@@ -11,7 +11,7 @@
  */
 var twoSum = function(nums, target) {
     for (var i = 0; i < nums.length; i++) {
-        for (var j = 1; j < nums.length; j++) {
+        for (var j = (i+1); j < nums.length; j++) {
             if (nums[i] + nums[j] == target) {
                 return [i, j];
             }
