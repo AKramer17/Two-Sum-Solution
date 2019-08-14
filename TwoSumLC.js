50142e7c0923586be155992239700d82ae962f04
Alex Kramer <kramer612@gmail.com>
2019-08-14 16:45:57 -0400

    completed

--- TwoSumLC.js
+++ TwoSumLC.js
@@ -11,7 +11,10 @@
  */
 var twoSum = function(nums, target) {
     for (var i = 0; i < nums.length; i++) {
-        for (var j = 0; j < nums.length; j++) {
+        for (var j = 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [i, j];
+            }
 
         }
     }
